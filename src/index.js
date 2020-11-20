f7c925294abd0957310cb80ac9c35c66448e1a55
gorobetsA <andreygorobez@gmail.com>
2020-11-20 12:06:53 +0200

    optimization with hoc

--- src/index.js
+++ src/index.js
@@ -8,22 +8,28 @@ const starWarsChars = [
     { name: 'Obi-Wan Kenobi ', side: 'light' }
 ]
 
-const App = ({list, side}) => {
+const App = ({list}) => (
+    <ul>
+        { list.map((char, index) => {
+            return (
+                <li key={char.name + index}>
+                    <strong>{char.name}</strong> - &nbsp;
+                    {char.side}
+                </li>
+            )
+        }) }
+    </ul>
+)
+
+const withFilteredProps = Component => ({list, side}) => {
     const filteredList = list.filter(char => char.side === side)
-    return (
-        <ul>
-            { filteredList.map((char, index) => {
-                return (
-                    <li key={char.name + index}>
-                        <strong>{char.name}</strong> - &nbsp;
-                        {char.side}
-                    </li>
-                )
-            }) }
-        </ul>
-    )
+    return <Component list={filteredList}/>
 }
 
+const FilteredList = withFilteredProps(App)
 
-
-ReactDOM.render(<App list={starWarsChars} side ="light"/>, document.getElementById('root'));
+ReactDOM
+    .render(
+        <FilteredList list={starWarsChars} side ="dark"/>,
+        document.getElementById('root')
+    );
